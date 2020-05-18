6cd994ad4ffb138a7aa9f1d5368e79c80475b034
Maverick Liberty <maverick.liberty29@gmail.com>
2020-05-17 23:09:12 -0400

    Phase_Compiler.bat: Use `python` and not `ppython`

--- Phase_Compiler.bat
+++ Phase_Compiler.bat
@@ -2,7 +2,7 @@
 title Cog Invasion Phase File Compiler
 
 set /p file=What phase file do you want compiled? 
-%CIOENGINE%\python\ppython.exe -B tool_Multifile.py --mtype compile --filename %file%
+%CIOENGINE%\python\python.exe -B tool_Multifile.py --mtype compile --filename %file%
 
 echo Done!
 pause >nul
